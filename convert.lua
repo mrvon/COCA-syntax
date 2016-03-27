03e1005feccd48f7b36b8844dca8d228559ec075
Dennis <mrvon@qq.com>
2016-03-27 20:30:06 +0800

    first version

---
+++ convert.lua
@@ -0,0 +1,15 @@
+local filename = "./main.coca"
+local file = assert(io.open(filename, "r"))
+local content = file:read("*a")
+io.close(file)
+
+local pattern = "(%d+) ([%a']+) ([%a])[ ]*[\n]*([^%d]+)[\n](%d+) %| (%d.%d%d)[%a ]*[\n]"
+
+for index, word, type, collocates, raw_freq, dispersion in string.gmatch(content, pattern) do
+    -- print("\t", word)
+    print("Index: ", index, word)
+    -- print("Type: ", type)
+    -- print("Collocates:\n", collocates)
+    -- print("Raw frequency: ", raw_freq)
+    -- print("Dispersion: ", dispersion)
+end
